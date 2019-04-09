6309889d660b887b31f880cc64ca904668aea174
Mahmoud Shepero <26594373+SheperoMah@users.noreply.github.com>
2019-04-09 16:12:26 +0200

    setup&dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EVSpatialChargingModel',
+      version='1.0.0',
+      description='This model estimates the charging load of EVs in a city.',
+      author='Mahmoud Shepero',
+      author_email='mahmoud.shepero@angstrom.uu.se',
+      url='https://github.com/SheperoMah/EVSpatialChargingModel',
+      packages=['spatialModelPkg'],
+      license='GNU General Public License v3.0'
+     )
