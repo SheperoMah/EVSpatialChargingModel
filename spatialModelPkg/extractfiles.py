a426fc63af9b2874c79b4c16b6698aedc30b5a39
Mahmoud Shepero <26594373+SheperoMah@users.noreply.github.com>
2019-04-09 15:48:12 +0200

    follow PEP8

--- spatialModelPkg/extractfiles.py
+++ spatialModelPkg/extractfiles.py
@@ -2,7 +2,7 @@
 import numpy as np
 import glob
 
-def readMatrixfiles(files, precision = 1000):
+def read_matrix_files(files, precision = 1000):
     """Extracts the tranistion matrix into a 3-d numpy array which can be used
     in the model.
 
@@ -39,12 +39,12 @@ if __name__ == "__main__":
 
     Example
     -------
-        $ python3 extractFiles.py './*.txt' 1000
+        $ python3 extractfiles.py './*.txt' 1000
     """
     import sys
     files = sys.argv[1]
     precision = int(sys.argv[2])
-    a = readMatrixfiles(files, precision)
+    a = read_matrix_files(files, precision)
     print(a)
     print("Shape of Matrix is " + str(a.shape[0]), str(a.shape[1]),
 str(a.shape[2]))
